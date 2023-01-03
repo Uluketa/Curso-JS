4fcf4e55f35d61093354f51d99ff5f4e6ddc691c
Lucas Miguel de Arruda <limirruda@gmail.com>
2023-01-03 20:15:48 -0300

    Express Session and Flash Messages

---
+++ node/aula15/src/controllers/homeController.js
@@ -0,0 +1,16 @@
+// const HomeModel = require('../models/HomeModel');
+
+// HomeModel.create({
+//     titulo: 'Titulo1',
+//     descricao: 'Descrição'
+// })
+//     .then(dados => console.log(dados)).
+//     catch(e => console.log(e));
+
+exports.paginaInicial = (req, res) => {
+    res.render('index');
+}
+
+exports.trataPosts = (req, res) => {
+    res.send(req.body);
+}
